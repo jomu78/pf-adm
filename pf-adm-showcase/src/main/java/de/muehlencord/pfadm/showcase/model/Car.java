2f105ee354990fc27c6ed06856b62f06429e5d96
Joern Muehlencord <joern.muehlencord@vitroconnect.com>
2025-08-03 14:06:32 +0200

    renamed to pf-adm

---
+++ pf-adm-showcase/src/main/java/de/muehlencord/pfadm/showcase/model/Car.java
@@ -0,0 +1,25 @@
+package de.muehlencord.pfadm.showcase.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * a car model
+ *
+ * @author Joern Muehlencord, 2025-05-05
+ * @since 0.1.0
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Car {
+
+  public String id;
+  public String brand;
+  public Integer year;
+  public String color;
+  public int price;
+  public boolean sold;
+
+}
