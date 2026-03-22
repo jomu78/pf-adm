b14d9c1cf5da0ec0dd1fc5553f9cf3eabe8f0ebc
Joern <dev@muehlencord.de>
2026-03-23 00:13:38 +0100

    support skin change

--- pf-adm-showcase/src/main/java/de/muehlencord/pfadm/showcase/view/ShowcaseLayoutView.java
+++ pf-adm-showcase/src/main/java/de/muehlencord/pfadm/showcase/view/ShowcaseLayoutView.java
@@ -1,11 +1,15 @@
 package de.muehlencord.pfadm.showcase.view;
 
 import de.muehlencord.pfadm.template.config.PfAdmConfig;
+import de.muehlencord.pfadm.template.config.SkinEnum;
 import de.muehlencord.pfadm.template.view.LayoutView;
+import jakarta.faces.model.SelectItem;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,9 +26,27 @@ import lombok.Setter;
 public class ShowcaseLayoutView extends LayoutView implements Serializable {
 
   private boolean borderless = false;
+  private final PfAdmConfig pfAdmConfig;
 
   @Inject
   public ShowcaseLayoutView(PfAdmConfig pfAdmConfig) {
     super(pfAdmConfig);
+    this.pfAdmConfig = pfAdmConfig;
+  }
+
+  public List<SelectItem> getAvailableSkins() {
+    return Arrays.stream(SkinEnum.values())
+      .map(skin -> new SelectItem(skin.getLabel(), toDisplayName(skin.getLabel())))
+      .toList();
+  }
+
+  public String getDefaultSkin() {
+    return pfAdmConfig.getSkin();
+  }
+
+  private String toDisplayName(String skinLabel) {
+    return skinLabel.replace("skin-", "")
+      .replace("-light", " light")
+      .replace('-', ' ');
   }
 }
